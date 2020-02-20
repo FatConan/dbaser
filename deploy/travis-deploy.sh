b4ac98d7c690abd39b7b85baa247bdb9c24868bd
Ian Usher <ian@codeplay.com>
2020-02-20 16:17:21 +0000

    Cat-ing the settings for debug

--- deploy/travis-deploy.sh
+++ deploy/travis-deploy.sh
@@ -1,3 +1,4 @@
 mkdir ~/.m2 -p
 echo "<settings><servers><server><id>github</id><username>${GITHUB_USERNAME}</username><password>${GITHUB_TOKEN}</password></server></servers></settings>" > ~/.m2/settings.xml
+cat ~/.m2/settings.xml
 mvn deploy
