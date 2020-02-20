9e681c21cffcac040690de9f9a145a2c10f2cfdf
Ian Usher <ian@codeplay.com>
2020-02-20 16:23:28 +0000

    Another push on travis

--- deploy/travis-deploy.sh
+++ deploy/travis-deploy.sh
@@ -1,4 +1,4 @@
-mkdir ~/.m2 -p
-echo "<settings><servers><server><id>github</id><username>${GITHUB_USERNAME}</username><password>${GITHUB_TOKEN}</password></server></servers></settings>" > ~/.m2/settings.xml
-cat ~/.m2/settings.xml
+mkdir $HOME/.m2 -p
+echo "<settings><servers><server><id>github</id><username>$GITHUB_USERNAME</username><password>$GITHUB_TOKEN</password></server></servers></settings>" > $HOME/.m2/settings.xml
+cat $HOME/.m2/settings.xml
 mvn deploy
